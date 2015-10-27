90a8ceb4126ed63645fc669ebdf613c94572d55b
Alberto-Vega <el_vega@live.com>
2015-10-27 11:05:39 -0700

    created a function that takes an arrays of Integers as a parameter and reverses it

--- ReverseArray.playground/Contents.swift
+++ ReverseArray.playground/Contents.swift
@@ -5,11 +5,19 @@ import UIKit
 var listOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 var tempArray = [Int]()
 
-for i in 1...listOfNumbers.count {
+
+func reverse(var arrayName: [Int]) -> [Int] {
+
+for i in 1...arrayName.count {
     print(" i is : \(i)")
-    print("The index is: \(listOfNumbers.count - i)")
-    tempArray.append(listOfNumbers[listOfNumbers.count - i])
+    print("The index is: \(arrayName.count - i)")
+    tempArray.append(arrayName[arrayName.count - i])
     print("\(tempArray)")
 }
 
-listOfNumbers = tempArray
+    arrayName = tempArray
+ 
+    return arrayName
+}
+
+reverse(listOfNumbers)
